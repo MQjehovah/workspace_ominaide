0a0233ce59bc9365434ae21ec73e84d790463b6a
MQjehovah <1421706030@qq.com>
2026-07-22 23:40:20 +0800

    fix(notif): 铃铛始终显示（不再仅在未读数>0时隐藏）

--- desktop/src/renderer/src/components/MainPanel.vue
+++ desktop/src/renderer/src/components/MainPanel.vue
@@ -122,8 +122,8 @@ onUnmounted(() => { if (notifTimer) clearInterval(notifTimer) })
           <span>在线</span>
         </div>
       </div>
-      <el-dropdown v-if="notifCount > 0" trigger="click" @visible-change="fetchNotifs">
-        <button class="bell-btn"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg><span class="bell-dot">{{ notifCount }}</span></button>
+      <el-dropdown trigger="click" @visible-change="fetchNotifs">
+        <button class="bell-btn"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg><span v-if="notifCount > 0" class="bell-dot">{{ notifCount }}</span></button>
         <template #dropdown>
           <el-dropdown-menu>
             <div v-for="n in notifs.slice(0,5)" :key="n.id" class="notif-item" @click="markRead(n)">
